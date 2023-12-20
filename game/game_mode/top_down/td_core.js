8499d87209b27391dffe47c5287a9551193dbb12
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2023-12-20 16:01:19 -0600

    added SV game
    Added the demo for the side view game segment

--- game/game_mode/top_down/td_core.js
+++ game/game_mode/top_down/td_core.js
@@ -10,6 +10,7 @@ function td_draw() {
     if(sceneState === "td") {
         //game functions are called here
         td_player_draw();
+        td_transistion_draw();
     }
 }
 
@@ -20,10 +21,16 @@ var top_down = function(sketch) {
 
         //game objects are called here
         td_player = new this.Sprite(400, 225, 50, 50);
+        td_player.rotationLock = true;
+
+        td_transistion = new this.Sprite(100, 400, 25, 25);
+        td_transistion.color = 'purple';
     }
 
     sketch.draw = function() {
         this.background("black");
+
+        td_player.overlaps(td_transistion);
 
         //for scene switching
         if(clear === true) {
