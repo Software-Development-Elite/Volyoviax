733e9c1718784153e1410cecb329899e017670eb
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2024-01-11 12:30:41 -0600

    Add files via upload

--- game/game_mode/user_interface/ui_core.js
+++ game/game_mode/user_interface/ui_core.js
@@ -1,7 +1,7 @@
 function ui_setup() {
     if(sceneState === "td" || sceneState === "sv") {
         new p5(user_interface);
-
+        console.log("hello");
         //TBD
     }
 }
@@ -12,17 +12,31 @@ function ui_draw() {
     }
 }
 
+var g = 0;
+var tim = 1
 var user_interface = function(sketch) {
     sketch.setup = function() {
         let screen = this.createCanvas(800, 450);
         screen.position(0,0);
 
         //game objects are called here
+        new this.Sprite(100,100,50,50);
     }
 
     sketch.draw = function() {
         //this.background("black");
-
+        if(this.kb.pressing("j")) {
+            for(g; g<1; g++) {
+                tim = new this.Sprite(300,300,25,25);
+            }
+            console.log(g);
+        }else {
+            g = 0;
+            if(tim !== 1) {
+                tim.remove();
+                this.clear();
+            }
+        }
         //for scene switching
         if(clear === true) {
             game_clear(sketch);
